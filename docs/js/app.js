8a9281a3bf72c237a76883304a020957628db1c7
Roman Ožana <roman@testomato.com>
2016-09-06 14:17:01 +0200

    Remove Google Code Prettify with Heighlight.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,6 +1,5 @@
 (function ($) {
 	return $(document).ready(function () {
-		$('pre > code').parent().addClass('prettyprint');
 		$('article table').addClass('table table-striped table-condensed');
 	});
 })(jQuery);
