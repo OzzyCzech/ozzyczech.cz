60227b8e774008b7f33ce63bb47fcd6dddf9a389
Roman Ožana <roman@ozana.cz>
2021-08-02 15:29:24 +0200

    Global await

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import path from 'path';
 import fs from "fs-extra";
-import { globby } from "globby";
+import {globby} from "globby";
 
 import { getPages } from "@sphido/core";
 import { pagination } from "@sphido/pagination";
@@ -64,7 +64,7 @@ renderer(
 );
 
 
-//(async () => {
+(async () => {
 
 	// Copy static content
 	let files = await globby(['static/**/*.*', 'content/**/*.*', '!**/*.{md,xml,html}', 'static/404.html']);
@@ -155,4 +155,4 @@ renderer(
 		)
 	);
 
-//})();
+})();
