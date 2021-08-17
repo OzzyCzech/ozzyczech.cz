cdd69430b462e38ff0953646797ea9706599f308
Roman Ožana <roman@ozana.cz>
2021-08-17 11:22:47 +0200

    CSSnano for production

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,10 @@
+const cssnano = require('cssnano')
+
 module.exports = {
 	plugins: [
 		require('postcss-import'),
 		require('tailwindcss'),
 		require('autoprefixer'),
+		process.env.VERCEL_ENV === 'production' ? cssnano({preset: 'default'}) : null,
 	]
 }
