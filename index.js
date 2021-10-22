34c6d62c83289e7f0c791c0fa7c15d801ebced53
Roman Ožana <roman@ozana.cz>
2021-10-22 13:34:10 +0200

    Upgrade

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 #!/usr/bin/env node --experimental-modules --experimental-loader @node-loader/babel
 
 import path from 'path';
-import fs from "fs-extra";
-import {globby} from "globby";
+import fs from 'fs-extra';
+import {globby} from 'globby';
 
-import Prism from "prismjs";
-import 'prismjs/components/prism-ini.js'
-import 'prismjs/components/prism-json.js'
-import 'prismjs/components/prism-jsx.js'
-import 'prismjs/components/prism-sql.js'
-import 'prismjs/components/prism-scss.js'
-import 'prismjs/components/prism-less.js'
-import 'prismjs/components/prism-css.js'
-import 'prismjs/components/prism-swift.js'
-import 'prismjs/components/prism-php.js'
-import 'prismjs/components/prism-php-extras.js'
-import 'prismjs/components/prism-markup-templating.js'
-import 'prismjs/components/prism-yaml.js'
-import 'prismjs/components/prism-python.js'
-import 'prismjs/components/prism-bash.js'
+import Prism from 'prismjs';
+import 'prismjs/components/prism-ini.js';
+import 'prismjs/components/prism-json.js';
+import 'prismjs/components/prism-jsx.js';
+import 'prismjs/components/prism-sql.js';
+import 'prismjs/components/prism-scss.js';
+import 'prismjs/components/prism-less.js';
+import 'prismjs/components/prism-css.js';
+import 'prismjs/components/prism-swift.js';
+import 'prismjs/components/prism-php.js';
+import 'prismjs/components/prism-php-extras.js';
+import 'prismjs/components/prism-markup-templating.js';
+import 'prismjs/components/prism-yaml.js';
+import 'prismjs/components/prism-python.js';
+import 'prismjs/components/prism-bash.js';
 
-import {getPages} from "@sphido/core";
-import {pagination} from "@sphido/pagination";
-import {link} from "@sphido/link";
-import {feed} from "@sphido/feed";
-import slugify from "@sindresorhus/slugify";
+import {getPages} from '@sphido/core';
+import {pagination} from '@sphido/pagination';
+import {link} from '@sphido/link';
+import {feed} from '@sphido/feed';
+import slugify from '@sindresorhus/slugify';
 
-import React from 'react'
-import {renderHTML, renderXML} from "./src/render.js";
+import React from 'react';
+import {renderHTML, renderXML} from './src/render.js';
 
-import Page from "./src/Page.js";
-import Pages from "./src/Pages.js";
-import PageTag from "./src/PageTag.js";
-import Sitemap from "./src/Sitemap.js";
+import Page from './src/Page.js';
+import Pages from './src/Pages.js';
+import PageTag from './src/PageTag.js';
+import Sitemap from './src/Sitemap.js';
 
-import {meta} from "@sphido/meta";
-import {frontmatter} from "@sphido/frontmatter";
-import {emoji} from "@sphido/emoji";
-import {markdown, options, renderer} from "@sphido/markdown"
+import {meta} from '@sphido/meta';
+import {frontmatter} from '@sphido/frontmatter';
+import {emoji} from '@sphido/emoji';
+import {markdown, options, renderer} from '@sphido/markdown';
 
 const domain = new URL('https://ozzyczech.cz/');
 
@@ -46,9 +46,9 @@ options(
 	{
 		highlight: (code, lang) => {
 			lang = lang || 'markup';
-			return Prism.highlight(code, Prism.languages[lang], lang)
+			return Prism.highlight(code, Prism.languages[lang], lang);
 		},
-	}
+	},
 );
 
 // @see https://marked.js.org/using_pro#renderer
@@ -67,7 +67,7 @@ renderer(
 			3: `<h3 id="${slugger.slug(raw)}" class="text-xl lg:text-3xl mb-4">${text}</h3>`,
 			4: `<h4 id="${slugger.slug(raw)}" class="lg:text-2xl mb-3">${text}</h4>`,
 			5: `<h5 id="${slugger.slug(raw)}" class="font-bold mb-3">${text}</h5>`,
-			6: `<h6 id="${slugger.slug(raw)}" class="font-bold mb-3">${text}</h6>`
+			6: `<h6 id="${slugger.slug(raw)}" class="font-bold mb-3">${text}</h6>`,
 		})[level],
 
 		image: (href, title, text) => {
@@ -79,13 +79,13 @@ renderer(
 								</a>		
 								<figcaption class="mt-2 italic text-gray-400 text-center">${title ? title : (text ? text : null)}</figcaption>
 						</figure>
-			</div>`
+			</div>`;
 		},
 
 		link: (href, title, text) => {
 			// Current domain
 			if (href.includes(domain.hostname)) {
-				return `<a href="${href}" title="${title ? title : ''}">${text}</a>`
+				return `<a href="${href}" title="${title ? title : ''}">${text}</a>`;
 			}
 
 			// YouTube video embed
@@ -93,7 +93,7 @@ renderer(
 				const video = new URL(href);
 				const id = video.searchParams.get('v');
 				if (id) {
-					return `<div class="aspect-w-16 aspect-h-9"><iframe src="https://www.youtube.com/embed/${id}?rel=0&controls=1" allowfullscreen></iframe></div>`
+					return `<div class="aspect-w-16 aspect-h-9"><iframe src="https://www.youtube.com/embed/${id}?rel=0&controls=1" allowfullscreen></iframe></div>`;
 				}
 			}
 
@@ -102,9 +102,9 @@ renderer(
 				// @see https://github.com/markedjs/marked/issues/458 - waiting for async
 			}
 
-			return `<a href="${href}" title="${title ? title : ''}" class="text-blue-600 dark:text-blue-400 hover:underline" target="_blank">${text}</a>`
-		}
-	}
+			return `<a href="${href}" title="${title ? title : ''}" class="text-blue-600 dark:text-blue-400 hover:underline" target="_blank">${text}</a>`;
+		},
+	},
 );
 
 
@@ -113,7 +113,7 @@ renderer(
 	// Copy static content
 	let files = await globby(['static/**/*.*', 'content/**/*.*', '!**/*.{md,xml,html}', 'static/404.html']);
 	for await (let file of files) {
-		await fs.copy(file, file.replace(/^[\w]+/, 'public'))
+		await fs.copy(file, file.replace(/^[\w]+/, 'public'));
 	}
 
 	// Get pages from directory
@@ -126,7 +126,7 @@ renderer(
 			meta,
 			(page) => page.tags.add('' + page.date.getFullYear()),
 			{link},
-		]
+		],
 	);
 
 	// Get sorted posts only
@@ -149,8 +149,8 @@ renderer(
 	for await (let page of pages) {
 		await renderHTML(
 			<Page page={page} tags={tags}/>,
-			path.join(page.dir.replace('content', 'public'), page.slug, 'index.html')
-		)
+			path.join(page.dir.replace('content', 'public'), page.slug, 'index.html'),
+		);
 	}
 
 	// index.json for https://fusejs.io/
@@ -161,8 +161,8 @@ renderer(
 				content: page.content.replace(/(<([^>]+)>)/ig, ''),
 				link: page.link(domain),
 				tags: [...page.tags],
-			})
-		)
+			}),
+		),
 	));
 
 	// tags pages
@@ -171,7 +171,7 @@ renderer(
 		await renderHTML(
 			<PageTag tag={tag.title} tags={tags} posts={posts.filter((post => post.tags.has(tag.title))).sort((a, b) => new Date(b.date) - new Date(a.date))}/>,
 			path.join('public/tag/', tag.slug, 'index.html'),
-		)
+		);
 	}
 
 	// TODO index tags render
@@ -181,7 +181,7 @@ renderer(
 	for await (const page of pagination(posts, 8)) {
 		await renderHTML(
 			<Pages posts={page.posts} current={page.current} pages={page.pages} tags={tags}/>,
-			page.current === 1 ? 'public/index.html' : path.join('public/page/', page.current.toString(), 'index.html')
+			page.current === 1 ? 'public/index.html' : path.join('public/page/', page.current.toString(), 'index.html'),
 		);
 	}
 
@@ -195,9 +195,9 @@ renderer(
 		'public/rss.xml',
 		feed(
 			posts.slice(0, 20),
-			{title: 'OzzyCzech.cz blog', description: 'Blog by Roman Ožana', link: 'https://ozzyczech.cz/',},
-			domain + 'rss.xml'
-		)
+			{title: 'OzzyCzech.cz blog', description: 'Blog by Roman Ožana', link: 'https://ozzyczech.cz/'},
+			domain + 'rss.xml',
+		),
 	);
 
 })();
