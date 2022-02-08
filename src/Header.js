68e8642e66db19fb77b77994c8b953ea08e73f92
Roman Ožana <roman@ozana.cz>
2022-02-08 17:25:02 +0100

    Remove Google Analytics

--- src/Header.js
+++ src/Header.js
@@ -10,22 +10,7 @@ export default ({title}) => {
 			<link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
 			<link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml"/>
 			<link rel="alternate" type="application/rss+xml" title="RSS" href="/rss.xml"/>
-
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/fuse.js/3.4.5/fuse.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/date-fns/2.0.0-alpha0/date_fns.min.js"></script>
-
-			<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132992179-1"></script>
-
-			<script
-				dangerouslySetInnerHTML={{
-					__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-132992179-1');
-          `,
-				}}
-			/>
 
 			<link rel="stylesheet" href="/css/style.css"/>
 			<script src="/js/app.js"></script>
