3bf0f253bb6f5a74451ba32266eb4e607c9cf246
Roman Ožana <roman@ozana.cz>
2022-04-29 13:32:49 +0200

    Fix react-dom error with server

--- src/render.js
+++ src/render.js
@@ -1,6 +1,6 @@
 import {outputFile} from 'fs-extra';
 import React from 'react';
-import {renderToStaticMarkup} from 'react-dom/server.js';
+import {renderToStaticMarkup} from 'react-dom/server';
 
 
 export async function renderHTML(element, file) {
