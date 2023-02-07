179ed847934ff2f09a0f51a6ce22fcdc5c7eacb0
Roman Ožana <roman@ozana.cz>
2023-02-07 08:11:12 +0100

    Sidebar fix

--- src/sidebar.js
+++ src/sidebar.js
@@ -5,15 +5,14 @@ export function getSidebarMenu(pages, active, className = '') {
 		if (page?.children) {
 			const expanded = active.startsWith(page.slug);
 			menu += `<li>
-	<button class="flex justify-between w-full px-3 py-2 rounded font-semibold hover:bg-neutral-200 dark:hover:bg-neutral-800" aria-expanded="${expanded}">
-		<span>${page.name}</span>
-		<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" class="fill-natural-500 dark:fill-neutral-400">
-			<path d="M0 0h24v24H0z" fill="none"/>
-			<path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
-		</svg>
-	</button>
-	${getSidebarMenu(page.children, active, expanded ? 'mx-2' : 'mx-2 hidden')}
-</li>`;
+				<button class="flex justify-between w-full px-3 py-2 rounded font-semibold hover:bg-neutral-200 dark:hover:bg-neutral-800" aria-expanded="${expanded}">
+					<span>${page.name}</span>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="24px" width="24px" class="fill-natural-500 dark:fill-neutral-400">
+						<path d="M0 0h24v24H0z" fill="none"/><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
+					</svg>
+				</button>
+				${getSidebarMenu(page.children, active, expanded ? 'mx-1.5 mt-1' : 'mx-1.5 hidden')}
+			</li>`;
 		} else {
 			menu += `<li>
 	<a href="${page.slug}" ${page.slug === active ? 'aria-current="page"' : ''} class="flex py-2 px-3 rounded hover:bg-neutral-200 dark:hover:bg-neutral-800 w-full ${page.slug == active ? 'font-semibold dark:bg-neutral-800 text-yellow-300' : ''}">
