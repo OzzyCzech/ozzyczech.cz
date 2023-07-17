9738d1d15f65e127680db3b7e4d8027831095254
Roman Ožana <roman@ozana.cz>
2023-07-17 09:20:14 +0200

    Remove dark as default

--- src/document.js
+++ src/document.js
@@ -4,7 +4,7 @@ import md5 from 'md5';
 
 export function getDocument({title, aside, main} = {}) {
   return `<!DOCTYPE html>
-<html lang="en" dir="ltr" class="dark">
+<html lang="en" dir="ltr">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
